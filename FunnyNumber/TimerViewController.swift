94f6d3a9ca1b69a553bbc6a09f5ab695fa817fec
staechap <mon.oracle@gmail.com>
2017-09-10 13:46:18 +0700

    UI OK

---
+++ FunnyNumber/TimerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TimerViewController.swift
+//  FunnyNumber
+//
+//  Created by Sunisa Kirtsopa on 9/10/2560 BE.
+//  Copyright © 2560 oramon. All rights reserved.
+//
+
+import UIKit
+
+class TimerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
